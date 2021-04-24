001557b79925690cea063f8536fe29b195dbe7d6
Maxim Frolov <buterbrodhs@gmail.com>
2021-04-24 19:24:44 +0300

    deploy

--- src/js/import/test-page.js
+++
@@ -1,6 +0,0 @@
-import { TestQuestions } from '../../blocks/modules/testPage/testQuestions/testQuestions';
-import { TestHeader } from '../../blocks/modules/testPage/testHeader/testHeader';
-
-new TestHeader();
-new TestQuestions();
-
